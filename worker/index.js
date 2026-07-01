11033cbb1f55801ea40b37ce114bcd1236792d1e
LVJ2026 <Laurence.Virbel-Jeandel@ac-nancy-metz.fr>
2026-07-01 14:43:22 +0200

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -4,38 +4,48 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/ecoles") {
-      return proxy(env, "ECOLES");
+
+      const reponse = await fetch(
+        `https://grist.numerique.gouv.fr/api/docs/${env.GRIST_DOCUMENT_ID}/tables/ECOLES/records`,
+        {
+          headers: {
+            Authorization: `Bearer ${env.GRIST_API_KEY}`
+          }
+        }
+      );
+
+      return new Response(await reponse.text(), {
+        status: reponse.status,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+
     }
 
     if (url.pathname === "/api/seances") {
-      return proxy(env, "SEANCES");
-    }
 
-    if (url.pathname === "/api/inscriptions") {
-      return proxy(env, "INSCRIPTIONS");
+      const reponse = await fetch(
+        `https://grist.numerique.gouv.fr/api/docs/${env.GRIST_DOCUMENT_ID}/tables/SEANCES/records`,
+        {
+          headers: {
+            Authorization: `Bearer ${env.GRIST_API_KEY}`
+          }
+        }
+      );
+
+      return new Response(await reponse.text(), {
+        status: reponse.status,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+
     }
 
     return env.ASSETS.fetch(request);
+
   }
 };
-
-async function proxy(env, table) {
-
-  const reponse = await fetch(
-    `https://grist.numerique.gouv.fr/api/docs/${env.GRIST_DOCUMENT_ID}/tables/${table}/records`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.GRIST_API_KEY}`
-      }
-    }
-  );
-
-  return new Response(await reponse.text(), {
-    status: reponse.status,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-
-}
